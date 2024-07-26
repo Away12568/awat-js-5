1db6d799ca847888ce4c1ec96d659f3fc4700600
awat <154649552+Away12568@users.noreply.github.com>
2024-07-26 11:27:45 +0500

    Add files via upload

---
+++ awat-js-1/main.js
@@ -0,0 +1,22 @@
+//  let a = prompt('как тебя зовут' )
+//  let b = prompt('сколько тебе лет')
+//  console.log('name:' + a);
+//  console.log('age: '+ b);
+
+// let a = prompt("сколько тебе лет ");
+
+// if (a >= 18) {
+//   console.log("тебе слишком мало лет");
+// } else if (a <= -1) {
+//   console.log("ты еше не в планах");
+// } else if (a <= 0) {
+//   console.log("ты еше не родился");
+// } else if (a < 18 || a > 99) {
+//   console.log("ты совершеннолетний");
+// }
+// else { "ты слишком стар"}
+
+let a = prompt( "какого вы года рождения") 
+
+
+console.log(2024 - a);
